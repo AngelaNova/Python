8aeff644f155145e5092ccb5baa45075926c053e
Angela Novakovic <160551855+AngelaNova@users.noreply.github.com>
2024-03-12 17:16:34 -0400

    Add files via upload
    Review of Django

---
+++ storefront/playground/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# request -> response
+# Request handlers 
+# action
+
+def say_hello(request):
+    x=1
+    y=2
+    return render(request, 'hello.htlm', {'name':'Mosh'})
